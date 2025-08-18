141102630bc11dd83f45eb6139b949ad9512f621
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-18 17:46:40 +0500

    add model

--- products/models/order.py
+++ products/models/order.py
@@ -4,9 +4,22 @@ from .product import Product
 
 
 class Order(models.Model):
+    PAYMENT_TYPE_CHOICES = [
+        ('full', 'Full payment'),
+        ('installment', 'Installment payment')
+    ]
+
+    INSTALLMENT_CHOICES = [
+        (3, '3'),
+        (6, '6'),
+        (9, '9'),
+        (12, '12'),
+    ]
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=13, validators=[phone_regex], unique=True)
-    is_paid = models.BooleanField(default=False)
+    payment_type = models.CharField(max_length=20, choices=PAYMENT_TYPE_CHOICES)
+    installment_months = models.PositiveIntegerField(choices=INSTALLMENT_CHOICES)
+    total_price = models.DecimalField(max_digits=5, decimal_places=3, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -17,6 +30,18 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=12, decimal_places=3)
 
     def __str__(self):
         return f'{self.product.title} x {self.quantity}'
+
+
+class InstallmentPayment(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="installments")
+    month_number = models.PositiveIntegerField()
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    due_date = models.DateField()
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.order} - {self.month_number}-oy ({'To‘langan' if self.is_paid else 'Kutilmoqda'})"
